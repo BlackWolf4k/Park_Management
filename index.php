5d189fe7677f131c072f20d226f71d6b4f9cfa19
BlackWolf4k <AfzdCOLssI@protonmail.com>
2023-01-18 22:07:01 +0100

    Added new pages with different functionalities

--- index.php
+++ index.php
@@ -1,113 +1,12 @@
-<?php
-	// Start the session
-	session_start();
-
-	// Connect to the database
-	include( "./connection/park_connect.php" );
-
-	if ( isset( $_REQUEST[ "add" ] ) )
-		if ( $_REQUEST[ "add" ] == 1 )
-		{
-			// Add the animal
-			// Clear the request
-			$_REQUEST = array();
-		}
-?>
 <!DOCTYPE html>
 <html>
 	<head>
 		<title>Park Menagement</title>
 	</head>
 	<body>
-		<form name = "new_animal_form" method = "post" action = "" >
-			<a>Select Park<a><br>
-			<select name = "park" onchange = "this.form.submit()" >
-				<option disabled selected value>Park</option>
-				<?php
-					// Get the parks
-					$query = "SELECT * from parks";
-					$result = mysqli_query( $park_connection, $query );
-
-					// Print all the parks
-					for ( $i = 0; $i < mysqli_num_rows( $result ); $i++ )
-					{
-						// Fetch the result
-						$fetched_result = mysqli_fetch_assoc( $result );
-
-						// Add the option
-						// Change the selected if anything was selected
-						echo "<option ";
-						if ( isset( $_REQUEST[ "park" ] ) )
-						{
-							if ( $_REQUEST[ "park" ] == $fetched_result["id"] )
-								echo "selected";
-						}
-						echo " value = '" . $fetched_result["id"] . "'>" . $fetched_result["name"] . "</option>";
-					}
-				?>
-			</select><br>
-			<a>Select Order</a><br>
-			<select name = "orders" onchange = "this.form.submit()" >
-				<option disabled selected value>Order</option>
-				<?php
-					// Get the orders
-					$query = "SELECT * from animals_orders";
-					$result = mysqli_query( $park_connection, $query );
-
-					// Print all the orders
-					for ( $i = 0; $i < mysqli_num_rows( $result ); $i++ )
-					{
-						// Fetch the result
-						$fetched_result = mysqli_fetch_assoc( $result );
-
-						// Add the option
-						// Change the selected if anything was selected
-						echo "<option ";
-						if ( isset( $_REQUEST[ "orders" ] ) )
-						{
-							if ( $_REQUEST[ "orders" ] == $fetched_result["id"] )
-								echo "selected";
-						}
-						echo " value = '" . $fetched_result["id"] . "'>" . $fetched_result["name"] . "</option>";
-					}
-				?>
-			</select><br>
-			<a>Select Species</a><br>
-			<select name = "species" onchange = "this.form.submit()" >
-				<option disabled selected value>Species</option>
-				<?php
-					if ( isset( $_REQUEST[ "orders" ] ) )
-					{
-						// Clear the input value
-						$order = mysqli_real_escape_string( $park_connection, $_REQUEST[ "orders" ] );
-
-						// Unset the request
-						// unset( $_REQUEST[ "orders" ] );
-
-						// Get the species
-						$query = "SELECT * from animals_species WHERE order_id='" . $order . "'";
-						$result = mysqli_query( $park_connection, $query );
-
-						// Print all the species
-						for ( $i = 0; $i < mysqli_num_rows( $result ); $i++ )
-						{
-							// Fetch the result
-							$fetched_result = mysqli_fetch_assoc( $result );
-
-							// Add the option
-							// Change the selected if anything was selected
-							echo "<option ";
-							if ( isset( $_REQUEST[ "species" ] ) )
-							{
-								if ( $_REQUEST[ "species" ] == $fetched_result["id"] )
-									echo "selected";
-							}
-							echo " value = '" . $fetched_result["id"] . "'>" . $fetched_result["name"] . "</option>";
-						}
-					}
-				?>
-			</select><br>
-			<input type = "submit" name = "add" value = "0" onclick = "this.value = '1'">
-		</form>
+		<h1>What do you want to do?</h1>
+		<a href = "./sub_pages/insert_animal.php" >Insert a animal</a><br>
+		<a href = "./sub_pages/animals_age.php" >Get animals medium age in a Park</a><br>
+		<a href = "./sub_pages/species_in_park.php" >Get species in a Park</a><br>
 	</body>
 </html>
---
+++ sub_pages/species_in_park.php
@@ -0,0 +1,86 @@
+<?php
+	// Start the session
+	session_start();
+
+	// Connect to the database
+	include( "../connection/park_connect.php" );
+
+	$results_page = 0;
+
+	if ( isset( $_REQUEST[ "add" ] ) )
+		if ( $_REQUEST[ "add" ] == "Add"
+			 && isset( $_REQUEST[ "park" ] )
+		   ) // Check all setted
+		{
+
+			// Change what to display
+			$results_page = 1;
+		}
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Park Menagement</title>
+	</head>
+	<body>
+	<?php
+	// $query = "SELECT animals.species_id, animals_species.name FROM animals LEFT JOIN animals_species ON animals.species_id = animals_species.id";
+	// $result = mysqli_query( $park_connection, $query );
+	// for ( $i = 0; $i < mysqli_num_rows( $result ); $i++ )
+	// 	echo mysqli_fetch_array( $result )["name"];
+	if ( $results_page != 1 )
+	{
+	?> 
+		<div class = "select_park_div" >
+			<a>View Park Species</a>
+			<form name = "select_park_form" method = "post" action = "" >
+				<a>Select Park<a><br>
+				<select name = "park" >
+					<option disabled selected value>Park</option>
+					<?php
+						// Get the parks
+						$query = "SELECT * from parks";
+						$result = mysqli_query( $park_connection, $query );
+
+						// Print all the parks
+						for ( $i = 0; $i < mysqli_num_rows( $result ); $i++ )
+						{
+							// Fetch the result
+							$fetched_result = mysqli_fetch_assoc( $result );
+
+							// Add the option
+							// Change the selected if anything was selected
+							echo "<option ";
+							if ( isset( $_REQUEST[ "park" ] ) )
+							{
+								if ( $_REQUEST[ "park" ] == $fetched_result["id"] )
+									echo "selected";
+							}
+							echo " value = '" . $fetched_result["id"] . "'>" . $fetched_result["name"] . "</option>";
+						}
+					?>
+				</select><br>
+				<input type = "submit" name = "add" value = "Add" >
+			</form>
+		</div>
+	<?php
+	}
+	else
+	{
+		$park_id = mysqli_real_escape_string( $park_connection, $_REQUEST[ "park" ] );
+
+		$query = "SELECT animals.species_id, animals_species.name FROM animals LEFT JOIN animals_species ON animals.species_id = animals_species.id WHERE animals.park_id = '" . $park_id . "'";
+		
+		echo $query;
+
+		$result = mysqli_query( $park_connection, $query );
+
+		for ( $i = 0; $i < mysqli_num_rows( $result ); $i++ )
+		echo mysqli_fetch_array( $result )["name"];
+
+		// Clear the request
+		$_REQUEST = array();
+	}
+	?>
+	</body>
+</html>
